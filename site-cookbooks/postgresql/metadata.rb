438ff5ac7add481f468abfd89fd9930fabaf71c6
Joseph Bridgwater-Rowe <joe@westernmilling.com>
2015-07-28 09:34:14 -0700

    Install Postgres 9.4

---
+++ site-cookbooks/postgresql/metadata.rb
@@ -0,0 +1,10 @@
+name             'postgresql'
+maintainer       'Joseph Bridgwater-Rowe'
+maintainer_email 'joe@westernmilling.com'
+license          'All rights reserved'
+description      'Installs/Configures postgresql'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'apt'
+depends 'tar'
