99f296e4864517a92fd8943802287e6f136aebc8
Joseph Bridgwater-Rowe <joe@westernmilling.com>
2015-08-17 17:48:06 -0700

    Recipe to install RabbitMQ
      Install RabbitMQ APT package direct from Rabbit.
    
      Add WIP promiscuous subscriber script

---
+++ site-cookbooks/rabbitmq/metadata.rb
@@ -0,0 +1,9 @@
+name             'rabbitmq'
+maintainer       'Joseph Bridgwater-Rowe'
+maintainer_email 'joe@westernmilling.com'
+license          'All rights reserved'
+description      'Installs/Configures rabbitmq'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'apt'
