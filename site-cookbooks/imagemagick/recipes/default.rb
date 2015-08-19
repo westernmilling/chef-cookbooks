643ff93ce917987d2071a01ae921c958c51b340c
Joseph Bridgwater-Rowe <joe@westernmilling.com>
2015-08-18 18:55:48 -0700

    Update ImageMagick version

--- site-cookbooks/imagemagick/recipes/default.rb
+++ site-cookbooks/imagemagick/recipes/default.rb
@@ -18,32 +18,33 @@ packages.each do |pkg|
 end
 
 remote_file "source" do
-  source 'http://www.imagemagick.org/download/ImageMagick-6.9.1-10.tar.gz'
-  path '/tmp/ImageMagick-6.9.1-10.tar.gz'
+  source 'http://www.imagemagick.org/download/releases/ImageMagick-6.9.2-0.tar.xz'
+  path '/tmp/ImageMagick-6.9.2-0.tar.xz'
   action :create
 end
 
-tar_extract '/tmp/ImageMagick-6.9.1-10.tar.gz' do
+tar_extract '/tmp/ImageMagick-6.9.2-0.tar.xz' do
   action :extract_local
   target_dir '/usr/src'
-  creates '/usr/src/ImageMagick-6.9.1-10'
+  compress_char ''
+  creates '/usr/src/ImageMagick-6.9.2-0'
 end
 
 execute './configure' do
   action :run
-  cwd '/usr/src/ImageMagick-6.9.1-10'
-  creates '/usr/src/ImageMagick-6.9.1-10/config.status'
+  cwd '/usr/src/ImageMagick-6.9.2-0'
+  creates '/usr/src/ImageMagick-6.9.2-0/config.status'
 end
 
 execute 'make' do
   action :run
-  cwd '/usr/src/ImageMagick-6.9.1-10'
-  creates '/usr/src/ImageMagick-6.9.1-10/magick/ImageMagick-6.Q16.pc'
+  cwd '/usr/src/ImageMagick-6.9.2-0'
+  creates '/usr/src/ImageMagick-6.9.2-0/magick/ImageMagick-6.Q16.pc'
 end
 
 execute 'make install' do
   action :run
-  cwd '/usr/src/ImageMagick-6.9.1-10'
+  cwd '/usr/src/ImageMagick-6.9.2-0'
   creates '/usr/local/bin/convert'
 end
 
