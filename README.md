6a487116d0ea66e16af124d293414fd37b60c7c5
Joseph Bridgwater-Rowe <joe@westernmilling.com>
2015-09-30 12:18:16 -0700

    Add libraries for imagemagick
      Add libraries required for imagemagick conversion to/from
      png, jpeg, and tiff.
    
      Downgrade version to 6.9.1-10 for stability.

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Random Chef cookbooks for application deployments
 
 ```
 knife solo prepare user@host
-knife solo cook -V -c knife.rb -o "recipe[x::y]" user@host
-knife solo cook -V -c knife.rb -o "role[role_name]" user@host
+knife solo cook -V -o "recipe[x::y]" user@host
+knife solo cook -V -o "role[role_name]" user@host
 knife solo cook -V -i private_key -o "recipe[???]"user@ip
 knife solo cook -V -c knife.rb -o "recipe_or_role[]" vagrant@ip -i private_key
 ```
--- site-cookbooks/imagemagick/recipes/default.rb
+++ site-cookbooks/imagemagick/recipes/default.rb
@@ -9,7 +9,7 @@
 include_recipe 'apt'
 include_recipe 'tar'
 
-packages = %w{ghostscript}
+packages = %w{ghostscript libjpeg-dev libpng-dev libtiff-dev}
 
 packages.each do |pkg|
   apt_package pkg do
@@ -18,33 +18,33 @@ packages.each do |pkg|
 end
 
 remote_file "source" do
-  source 'http://www.imagemagick.org/download/releases/ImageMagick-6.9.2-0.tar.xz'
-  path '/tmp/ImageMagick-6.9.2-0.tar.xz'
+  source 'http://www.imagemagick.org/download/releases/ImageMagick-6.9.1-10.tar.xz'
+  path '/tmp/ImageMagick-6.9.1-10.tar.xz'
   action :create
 end
 
-tar_extract '/tmp/ImageMagick-6.9.2-0.tar.xz' do
+tar_extract '/tmp/ImageMagick-6.9.1-10.tar.xz' do
   action :extract_local
   target_dir '/usr/src'
   compress_char ''
-  creates '/usr/src/ImageMagick-6.9.2-0'
+  creates '/usr/src/ImageMagick-6.9.1-10'
 end
 
 execute './configure' do
   action :run
-  cwd '/usr/src/ImageMagick-6.9.2-0'
-  creates '/usr/src/ImageMagick-6.9.2-0/config.status'
+  cwd '/usr/src/ImageMagick-6.9.1-10'
+  creates '/usr/src/ImageMagick-6.9.1-10/config.status'
 end
 
 execute 'make' do
   action :run
-  cwd '/usr/src/ImageMagick-6.9.2-0'
-  creates '/usr/src/ImageMagick-6.9.2-0/magick/ImageMagick-6.Q16.pc'
+  cwd '/usr/src/ImageMagick-6.9.1-10'
+  creates '/usr/src/ImageMagick-6.9.1-10/magick/ImageMagick-6.Q16.pc'
 end
 
 execute 'make install' do
   action :run
-  cwd '/usr/src/ImageMagick-6.9.2-0'
+  cwd '/usr/src/ImageMagick-6.9.1-10'
   creates '/usr/local/bin/convert'
 end
 
